ec7ddc31668c260d8e1c148e39091fe52cb15006
Abdeljalil Salhi <abdousalhi868@gmail.com>
2022-07-23 15:51:01 +0100

    push the rush00

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "rush01.c"
+
+int main() {
+    rush(5, 3);
+    ft_putchar('\n');
+    rush(5, 1);
+    ft_putchar('\n');
+    rush(1, 1);
+    ft_putchar('\n');
+    rush(1, 5);
+    ft_putchar('\n');
+    rush(4, 4);
+    return 0;
+}
---
+++ rush01.c
@@ -0,0 +1,48 @@
+#include "ft_putchar.c"
+
+void    printer(int x, char start, char middle, char end)
+{
+    int i;
+
+    i = 1;
+    while (i <= x)
+    {
+        if (i == 1)
+            // We are at the start of the line
+            ft_putchar(start);
+        else if (i == x)
+            // We are at the end of the line
+            ft_putchar(end);
+        else
+            // We are in the middle of the line
+            ft_putchar(middle);
+        i++;
+    }
+    ft_putchar('\n');
+}
+
+void    tester(int x, int y, int j)
+{
+    // We call the printer function for the header
+    if (j == 1)
+        printer(x, '/', '*', '\\');
+    // We call the printer function for the footer
+    else if (j == y && y != 1)
+        printer(x, '\\', '*', '/');
+    // We call the printer function for the body
+    else
+        printer(x, '*', ' ', '*');
+}
+
+void    rush(int x, int y)
+{
+    int j;
+
+    j = 1;
+    // Iterate through all the lines
+    while (j <= y)
+    {
+        tester(x, y, j);
+        j++;
+    }
+}
